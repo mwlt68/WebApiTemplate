3e11ab246a18510ebc02ee1a6c19dd907f1dfb11
mwlt68 <mwltgr@gmail.com>
2022-12-10 20:45:06 +0300

    Authentication controller add

---
+++ Template.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService authenticationService;
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            this.authenticationService = authenticationService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync([FromBody] UserLoginRequestDto model)
+        {
+            var authenticateResult = await authenticationService.AuthenticateAsync(model.username, model.password);
+            return Ok(authenticateResult);
+        }
+    }
+}
--- Template.API/Program.cs
+++ Template.API/Program.cs
@@ -1,11 +1,26 @@
+using Business.Mapping;
+using Core.Extensions;
+using Core.Utilities.Security.Token.Jwt;
+using DataAccess.Abstracts;
+using DataAccess.Concreate.Contexts;
+using DataAccess.Concreate.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+builder.Services.Configure<JwtSettings>(jwtSettingsSection);
+builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCustomSwaggerGen();
+var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+builder.Services.AddCustomJwtToken(jwtSettings);
+builder.Services.AddCustomAutoMapper(new GeneralMapping());
+builder.Services.AddSingleton<IJwtService, JwtService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<Business.Abstract.IAuthenticationService, Business.Concreate.AuthenticationService>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
